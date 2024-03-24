417d0f4003b629e68cc439cb351abe849bf86968
rosemulazada <rosemulazada16@gmail.com>
2024-03-24 04:04:08 +0100

    INIT: Hello, world!

--- src/app.js
+++ src/app.js
@@ -1,21 +1,30 @@
+// core modules
 const path = require("path");
+
+// npm modules
 const express = require("express");
 const ejs = require("ejs");
 
 const app = express();
+const port = 8000;
 
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
+// Setup handlebars engine and views location
 app.set("view engine", "ejs");
 app.set("views", viewsPath);
 
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
     res.render("index");
 });
 
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000.");
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
 });
+
+module.exports = app;
